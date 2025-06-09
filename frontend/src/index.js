5225a4760a4a5ab936f86fdd9397967e9bc3b153
lucas <>
2025-06-09 19:02:17 -0300

    FEAT: implementando json para deploy na vercel

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,3 +11,4 @@ root.render(<App />); // Removido o React.StrictMode
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
